ee91cac3d977e8025dcd7c595d059f8f56028d43
bololomil <113362334+bololomil@users.noreply.github.com>
2023-12-11 08:24:13 -0300

    Add files via upload

--- percelas.js
+++ percelas.js
@@ -1,1 +1,18 @@
-qual
+const frm = document.querySelector("form")
+const resp =document.querySelector("pre")
+
+//ouvinte
+frm.addEventListener("submit" , (e)=>{
+    e.preventDefault()
+
+    const valopr = Number(frm.invalor.value)
+    const num = Number(frm.inpalcela.value)
+    const valorParcela = Math.floor(valor /num)
+    let valorFinal = valorParcela + (valor % num)
+    let reposta= ""
+    for(let i = 1; i < num; i++){
+        resposta = reposta + i + "ª Parcela: R$" + valorParcela.toFixed(2) + "\n"
+    }
+    resposta = resposta + num + "ª Parcel: R$" + valorFinal.toFixed(2)
+    resp.innerText= resposta
+})
